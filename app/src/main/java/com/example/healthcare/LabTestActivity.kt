370fc249f1df830f14a19643c452ed372edeac35
Harsha <harshabs113@gmail.com>
2023-06-15 23:15:32 +0530

    feat: Implemeted LabTest and LabTestDetails Activity

---
+++ app/src/main/java/com/example/healthcare/LabTestActivity.kt
@@ -0,0 +1,88 @@
+package com.example.healthcare
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SimpleAdapter
+import kotlinx.android.synthetic.main.activity_lab_test.*
+
+class LabTestActivity : AppCompatActivity() {
+
+    private val packages= arrayOf(
+        arrayOf("Package 1: Full Body Checkup","", "", "","999"),
+        arrayOf("Package 2: Blood Glucose Fasting","", "", "","299"),
+        arrayOf("Package 3: COVID-19 Antibody - Ig6","", "", "","899"),
+        arrayOf("Package 4: Thyroid Check","", "", "","499"),
+        arrayOf("Package 5: Immunity Check","", "", "","699")
+    )
+
+    private val packagesDetails= arrayOf(
+        "Blood Glucose Fasting\n"+
+                "Complete Hemogram\n"+
+                "HbA1c\n"+"Iron Studies\n"+
+                "Kidney Function test\n"+
+                "LDH Lactate Dehydrogenase, Serum\n"+
+                "Lipid Profile\n"+
+                "Liver Function Test",
+        "Blood Glucose Fasting",
+        "COVID-19 Antibody - Ig6",
+        "Thyroid Profile-Total (T3, T4 & TSH Ultra-sensitive)",
+        "Complete Hemogram\n"+
+                "CRP (C Reactive Protein) Quantitative, Serum\n"+
+                " Iron studies\n "+
+                "Kidney Function test\n"+
+                "Vitamin D Total -25 Hydroxy\n"+
+                "Liver Function test\n"+
+                "Lipid profile"
+    )
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab_test)
+
+        buttonLBBack.setOnClickListener {
+            val intent= Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+
+       val list = mutableListOf<MutableMap<String, String>>()
+        for(i in 0 until packages.size){
+            val item = mutableMapOf<String, String>()
+            item["line1"] = packages[i][0]
+            item["line2"] = packages[i][1]
+            item["line3"] = packages[i][2]
+            item["line4"] = packages[i][3]
+            item["line5"] = "Total Fees:"+packages[i][4]+"/-"
+            list.add(item)
+        }
+
+        val sa= SimpleAdapter(
+            this,
+            list,
+            R.layout.multi_lines,
+            arrayOf("line1", "line2", "line3", "line4", "line5"),
+            intArrayOf(R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e)
+        )
+
+
+       // set adapter on the list
+        listViewLB.adapter = sa
+
+        listViewLB.setOnItemClickListener { adapterView, view, i, l ->
+            val intent = Intent(this,LabTestDetailsActivity::class.java)
+            intent.putExtra("text1", packages[i][0])
+            intent.putExtra("text2", packagesDetails[i])
+            intent.putExtra("text3", packages[i][4])
+            startActivity(intent)
+        }
+
+
+
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/healthcare/LabTestDetailsActivity.kt
@@ -0,0 +1,75 @@
+package com.example.healthcare
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_lab_test_details.*
+
+class LabTestDetailsActivity : AppCompatActivity() {
+
+    //create variables/instance
+    private lateinit var pkgName: TextView
+    private lateinit var totalCost: TextView
+    private lateinit var lbDetails: EditText
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab_test_details)
+
+        totalCost=findViewById(R.id.textViewLBDTCost)
+        pkgName=findViewById(R.id.textViewLBDPkgName)
+        lbDetails=findViewById(R.id.editTextLBD)
+
+//        lbDetails.keyListener=null
+
+        //Retrieve Data from  intent
+        val it = intent
+        val pkgNameInit = it.getStringExtra("text1")
+        val lbDetailsInit = it.getStringExtra("text2")
+        val totalCostInit = it.getStringExtra("text3")
+
+        //assign intent data
+        // Editable obj created from String, since string cannot be assigned to Editable variables
+        pkgName.text =pkgNameInit
+        lbDetails.text = Editable.Factory.getInstance().newEditable(lbDetailsInit)
+        totalCost.text = Editable.Factory.getInstance().newEditable("Fess:$totalCostInit/-")
+
+        buttonLBBack.setOnClickListener {
+            val it = Intent(this,LabTestActivity::class.java)
+            startActivity(it)
+        }
+
+        buttonLBAddCart.setOnClickListener {
+            val sharedpref = getSharedPreferences ("shared_prefs", MODE_PRIVATE)
+            val username =sharedpref.getString("username","").toString()
+
+            val product=pkgName.text.toString()
+//            val price= Float.parseFloat(intent.getStringExtra("text3").toString())
+            val price = intent.getStringExtra("text3")?.toFloatOrNull() ?: 0.0f
+
+            //        created DataBase instance
+            val db = DataBase(this,"healthcare",null,1)
+
+            if(db.checkCart(username,product)==1){
+                Toast.makeText(this,"Product Already Added",Toast.LENGTH_SHORT).show()
+            }else {
+                db.addCart(username,product,price,"lab")
+                Toast.makeText(this,"Record Inserted to Cart",Toast.LENGTH_SHORT).show()
+                val it = Intent(this,LabTestActivity::class.java)
+                startActivity(it)
+            }
+
+        }
+
+
+
+    }
+}
